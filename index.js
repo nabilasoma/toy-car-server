8ead1dd9dbde5b1ed66302ffdbed0fecaa717c97
unknown <nabilasoma@gmail.com>
2023-05-18 23:43:15 +0600

    added new data collections

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const cors = require('cors');
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const app = express();
 require('dotenv').config();
 const port = process.env.PORT || 5000;
@@ -30,6 +30,50 @@ async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
+
+
+const toyCollection = client.db('toyWorld').collection('allToys');
+const addToyCollection = client.db('toyWorld').collection('addNew')
+
+
+
+app.get('/allToys', async(req, res) => {
+    const cursor = toyCollection.find();
+    const result = await cursor.toArray();
+    res.send(result);
+})
+
+app.get('/allToys/:id', async(req, res) => {
+    const id = req.params.id;
+    const query = {_id: new ObjectId(id)}
+
+    const options = {
+        // Include only the `title` and `imdb` fields in the returned document
+        projection: {quantity: 1, name: 1, rating: 1,  seller_name: 1, picture: 1, seller_email: 1, price: 1, description: 1 },
+      };
+  
+    const result = await toyCollection.findOne(query, options);
+    res.send(result)
+})
+
+
+// addNewToy
+
+app.post('/addNew', async(req, res) => {
+    const addToy = req.body;
+    console.log(addToy);
+    const result = await addToyCollection.insertOne(addToy);
+    res.send(result)
+})
+
+
+app.get('/addNew', async(req, res) => {
+    const cursor = addToyCollection.find();
+    const result = await cursor.toArray();
+    res.send(result);
+})
+
+
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
